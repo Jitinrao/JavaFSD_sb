11f47a730b8f337857854f48e3dd6733b11505f5
Jitender <jitinrao489@gmail.com>
2025-06-26 11:26:13 +0530

    Update EmployeeController.java

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,4 +1,22 @@
-package com.example.demo.controller;
+package com.ems.jims_ems.controller;
 
-public class EmployeeController {
+import com.ems.jims_ems.model.Employee;
+import com.ems.jims_ems.services.EmpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ems")
+public class EmpController {
+
+    @Autowired
+    public EmpService empService;
+
+    @PostMapping
+    public Employee createEmp(@RequestBody Employee emp) {
+        return empService.saveEmp(emp);
+    }
 }
